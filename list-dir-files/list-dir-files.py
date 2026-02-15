19ced7d808dc6a090328ca2be8aacb3b54452648
ofurufu <ayodeleadewuyia+github@gmail.com>
2026-02-15 17:24:31 +0100

    Exception Handling to catch any user input error.

--- list-dir-files/list-dir-files.py
+++ list-dir-files/list-dir-files.py
@@ -2,12 +2,20 @@
 import os
 
 # User enters dir(s) to see content
-folders = input("Please provide list of dir(s) with spaces inbetween: ").split()
+dirs = input("Please provide list of dir(s) with spaces inbetween: ").split()
 
 # Looping through the dir(s)
-for folder in folders:
-    files = os.listdir(folder)
-    print(f"====== Listing files in {folder} folder =====")
+for dir in dirs:
+    try:
+        files = os.listdir(dir)
+    except FileNotFoundError:
+        print(f"Please enter a valid dir name in stead of {dir}.")
+        continue
+    except PermissionError:
+        print("You do not have permission to view content in this dir")
+        continue
+    
+    print(f"====== Listing files in {dir} =====")
     
     for file in files:
         print(file)
