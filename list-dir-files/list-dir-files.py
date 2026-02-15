66b0a72e85b8371be1e10ee859c045fd67aaff5b
ofurufu <ayodeleadewuyia+github@gmail.com>
2026-02-15 16:42:34 +0100

    Imported os to interact with the system and list files in folders.

--- list-dir-files/list-dir-files.py
+++ list-dir-files/list-dir-files.py
@@ -1,6 +1,10 @@
+# Import os module to interact with the system
+import os
+
 # User enters dir(s) to see content
-folders = input("Please provide list of dirs with spaces inbetween: ").split()
+folders = input("Please provide list of dir(s) with spaces inbetween: ").split()
 
 # Looping through the dir(s)
 for folder in folders:
-    print(folder)
+    file = os.listdir(folder)
+    print(file)
