300851eaf4aca5866f7b29bb611245f0e1363ac5
ofurufu <ayodeleadewuyia+github@gmail.com>
2026-02-15 16:35:49 +0100

    Initial commit

---
+++ list-dir-files/list-dir-files.py
@@ -0,0 +1,6 @@
+# User enters dir(s) to see content
+folders = input("Please provide list of dirs with spaces inbetween: ").split()
+
+# Looping through the dir(s)
+for folder in folders:
+    print(folder)
